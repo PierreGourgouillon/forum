df3ee69f03342c4346b4a09b90ff2307a64cf5dd
PierreGourgouillon <72550004+PierreGourgouillon@users.noreply.github.com>
2021-05-18 17:33:39 +0200

    Creation of tables is finish, we need to change someone string in to array

--- Back-end/Server/database.go
+++ Back-end/Server/database.go
@@ -4,8 +4,48 @@ import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
 import "fmt"
 
+const tableIdentity = `CREATE TABLE IF NOT EXISTS identity(
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+user_email VARCHAR(50) NOT NULL,
+user_pseudo VARCHAR(50) NOT NULL,
+user_password VARCHAR(50) NOT NULL )`
+
+const tableProfile = `CREATE TABLE IF NOT EXISTS profile(
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+user_location VARCHAR(50),
+user_date DATE NOT NULL,
+user_image BINARY,
+user_bio TEXT )`
+
+const tablePost = `CREATE TABLE IF NOT EXISTS post(
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+posts_id INT)` // A modifier pour avoir un tableau
+
+const tableCommentary = `CREATE TABLE IF NOT EXISTS commentary(
+commentary_id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+post_id INT,
+commentary_date DATE,
+commentary_message TEXT)`
+
+const tableAllPosts = `CREATE TABLE IF NOT EXISTS allposts (
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+user_pseudo VARCHAR(50),
+user_message TEXT,
+user_categories VARCHAR(100),
+post_image BINARY, post_date DATE,
+post_hour VARCHAR(20),
+post_likes INT,
+post_dislikes INT,
+post_commentaries VARCHAR(200))`
+
+const tableCategory = `CREATE TABLE IF NOT EXISTS category(
+category_id INT AUTO_INCREMENT PRIMARY KEY,
+category_name VARCHAR(50))`
+
 func main() {
-	createAndSelectDB("testD")
+	createAndSelectDB("Forum")
+	createTable("Forum")
 }
 
 func createAndSelectDB(name string) {
@@ -18,7 +58,7 @@ func createAndSelectDB(name string) {
 	defer db.Close()
 
 	//Création de bdla
-	_, error := db.Exec("CREATE DATABASE " + name)
+	_, error := db.Exec("CREATE DATABASE IF NOT EXISTS " + name)
 
 	if error != nil {
 		panic(err)
@@ -32,5 +72,24 @@ func createAndSelectDB(name string) {
 		panic(err)
 	} else {
 		fmt.Println("DB selected successfully..")
+	}
+}
+
+func createTable(nameDb string) {
+	db, err := sql.Open("mysql", "root:pierre@(127.0.0.1:49153)/"+nameDb)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	tableau := [6]string{tableIdentity, tableProfile, tablePost, tableAllPosts, tableCategory, tableCommentary}
+	for j := range tableau {
+		_, error := db.Exec(tableau[j]) //A changer le mdp par la suite
+
+		if error != nil {
+			panic(error)
+		} else {
+			fmt.Println("Table Identity is create")
+		}
 	}
 }
