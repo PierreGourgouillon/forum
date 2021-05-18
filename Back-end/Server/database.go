6e034d0ee108a94d1d700990a49d053986a3b77b
PierreGourgouillon <72550004+PierreGourgouillon@users.noreply.github.com>
2021-05-19 00:31:42 +0200

    Modification db and add a table

--- Back-end/Server/database.go
+++ Back-end/Server/database.go
@@ -1,47 +1,11 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"strconv"
+)
 import _ "github.com/go-sql-driver/mysql"
 import "fmt"
-
-const tableIdentity = `CREATE TABLE IF NOT EXISTS identity(
-user_id INT AUTO_INCREMENT PRIMARY KEY,
-user_email VARCHAR(50) NOT NULL,
-user_pseudo VARCHAR(50) NOT NULL,
-user_password VARCHAR(50) NOT NULL )`
-
-const tableProfile = `CREATE TABLE IF NOT EXISTS profile(
-user_id INT AUTO_INCREMENT PRIMARY KEY,
-user_location VARCHAR(50),
-user_date DATE NOT NULL,
-user_image BINARY,
-user_bio TEXT )`
-
-const tablePost = `CREATE TABLE IF NOT EXISTS post(
-user_id INT AUTO_INCREMENT PRIMARY KEY,
-posts_id INT)` // A modifier pour avoir un tableau
-
-const tableCommentary = `CREATE TABLE IF NOT EXISTS commentary(
-commentary_id INT AUTO_INCREMENT PRIMARY KEY,
-user_id INT,
-post_id INT,
-commentary_date DATE,
-commentary_message TEXT)`
-
-const tableAllPosts = `CREATE TABLE IF NOT EXISTS allposts (
-user_id INT AUTO_INCREMENT PRIMARY KEY,
-user_pseudo VARCHAR(50),
-user_message TEXT,
-user_categories VARCHAR(100),
-post_image BINARY, post_date DATE,
-post_hour VARCHAR(20),
-post_likes INT,
-post_dislikes INT,
-post_commentaries VARCHAR(200))`
-
-const tableCategory = `CREATE TABLE IF NOT EXISTS category(
-category_id INT AUTO_INCREMENT PRIMARY KEY,
-category_name VARCHAR(50))`
 
 func main() {
 	createAndSelectDB("Forum")
@@ -57,7 +21,7 @@ func createAndSelectDB(name string) {
 	}
 	defer db.Close()
 
-	//Création de bdla
+	//Création de la bd
 	_, error := db.Exec("CREATE DATABASE IF NOT EXISTS " + name)
 
 	if error != nil {
@@ -76,20 +40,67 @@ func createAndSelectDB(name string) {
 }
 
 func createTable(nameDb string) {
+	//Connexion à la bd sur le docker
 	db, err := sql.Open("mysql", "root:pierre@(127.0.0.1:49153)/"+nameDb)
 
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
-	tableau := [6]string{tableIdentity, tableProfile, tablePost, tableAllPosts, tableCategory, tableCommentary}
+
+	//Création des tables
+	tableau := [7]string{tableUserIdentity, tableUserProfile, tableUserPost, tableAllPosts, tableCategories, tableAllCommentary, tablePostCategory}
 	for j := range tableau {
 		_, error := db.Exec(tableau[j]) //A changer le mdp par la suite
 
 		if error != nil {
 			panic(error)
 		} else {
-			fmt.Println("Table Identity is create")
+			fmt.Println("Table " + strconv.Itoa(j+1) + " is create")
 		}
 	}
 }
+
+const tableUserIdentity = `CREATE TABLE IF NOT EXISTS userIdentity(
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+user_email VARCHAR(50) NOT NULL,
+user_pseudo VARCHAR(30) NOT NULL,
+user_password VARCHAR(50) NOT NULL,
+user_birth DATE NOT NULL)`
+
+const tableUserProfile = `CREATE TABLE IF NOT EXISTS userProfile(
+user_id INT,
+user_location VARCHAR(50) NULL,
+user_image BINARY NULL,
+user_bio TEXT NULL )`
+
+const tableUserPost = `CREATE TABLE IF NOT EXISTS userPost(
+user_id INT,
+posts_id INT NOT NULL )`
+
+const tableAllCommentary = `CREATE TABLE IF NOT EXISTS allCommentary(
+commentary_id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+post_id INT NOT NULL,
+commentary_date DATE NOT NULL,
+commentary_message TEXT NULL )`
+
+const tableAllPosts = `CREATE TABLE IF NOT EXISTS allPosts (
+post_id INT AUTO_INCREMENT PRIMARY KEY,
+user_pseudo VARCHAR(50) NOT NULL,
+user_id INT NOT NULL,
+user_message TEXT NULL,
+post_image BINARY,
+post_date DATE NULL,
+post_hour TIME NOT NULL,
+post_likes INT NULL,
+post_dislikes INT NULL)`
+
+const tableCategories = `CREATE TABLE IF NOT EXISTS categories(
+category_id INT AUTO_INCREMENT PRIMARY KEY,
+category_name VARCHAR(50) NOT NULL)`
+
+const tablePostCategory = `CREATE TABLE IF NOT EXISTS postCategory (
+post_id INT NOT NULL,
+category_id INT NULL,
+post_category VARCHAR(50)NULL)`
