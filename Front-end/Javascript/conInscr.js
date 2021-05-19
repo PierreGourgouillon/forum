48f340369ae4ac5fd4eee93ec8032028323b201a
TGAULE <thomas.gaule@ynov.com>
2021-05-19 10:50:15 +0200

    merge page demarrage | connect | inscription

---
+++ Front-end/Javascript/conInscr.js
@@ -0,0 +1,38 @@
+function Afficher1()
+{ 
+var input = document.getElementById("paul1"); 
+if (input.type === "password")
+{ 
+input.type = "text"; 
+} 
+else
+{ 
+input.type = "password"; 
+} 
+} 
+
+function Afficher2()
+{ 
+var input = document.getElementById("paul2"); 
+if (input.type === "password")
+{ 
+input.type = "text"; 
+} 
+else
+{ 
+input.type = "password"; 
+} 
+} 
+
+function Afficher3()
+{ 
+var input = document.getElementById("paul3"); 
+if (input.type === "password")
+{ 
+input.type = "text"; 
+} 
+else
+{ 
+input.type = "password"; 
+} 
+} 
