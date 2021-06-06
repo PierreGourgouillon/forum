ce48581da38a71e54ac9e4a6d22cf26d75f442a0
tggaule <thomas.gaule@ynov.com>
2021-06-06 08:03:52 -0700

    ajout d'une image au post

--- tweet_perso/scriptpost.js
+++ tweet_perso/scriptpost.js
@@ -1,5 +1,7 @@
 const txt_area = document.getElementById("textArea")
 const inputForm = document.getElementById("inputForm")
+const inputImage = document.getElementById("file1")
+const inputGif = document.getElementById("file2")
 let id_post = 0
 
 inputForm.addEventListener("click",function(){
@@ -7,77 +9,102 @@ inputForm.addEventListener("click",function(){
     inputForm.style.background = "rgb(72, 45, 116)";
     setTimeout(() => {
         inputForm.style.background = "rgb(121, 75, 196)";
-        
-        console.log(inputForm.style.background)
     }, 120);   
     CreatNewPost()
 })
-
-function CreatNewPost (){
-    if (txt_area.value != ""){
-        /*div general*/
-        let NewPost 
-        NewPost = document.createElement('div')
-        NewPost.id = `post${id_post}`
-        NewPost.classList.add("tweet")
-        document.getElementById('zonePostTweet').appendChild(NewPost)
-
-            /*div image profile*/
-            let pp
-            pp = document.createElement('div')
-            pp.classList.add("pp")
-            NewPost.appendChild(pp)
-
-            /*div pseudo*/
-            let name
-            name = document.createElement('div')
-            name.classList.add("name")
-            name.innerHTML = `@jesuis${id_post}`
-            NewPost.appendChild(name)
-
-            /*div logo pioutter*/
-            let logo
-            logo= document.createElement('img')
-            logo.classList.add("logo")
-            logo.src="/tweet-perso/img/piout.png"
-            document.getElementById(NewPost.id).appendChild(logo)
-
-            /*div contenue du tweet*/
-            let content
-            content =document.createElement('div')
-            content.classList.add("contenue")
-            content.innerHTML = txt_area.value
-            NewPost.appendChild(content)
-
-        /*div separartion*/
-        let separation
-        separation= document.createElement('div')
-        separation.classList.add("separation")
-        document.getElementById(NewPost.id).appendChild(separation)
-
-            /*div like*/
-            let like
-            like= document.createElement('img')
-            like.classList.add("like")
-            like.src="/tweet-perso/img/iconCoeurLike.png"
-            like.id = `post${id_post}`
-            document.getElementById(NewPost.id).appendChild(like)
-
-
-            /*div commentaire*/
-            let commentaire
-            commentaire= document.createElement('img')
-            commentaire.classList.add("commentaire")
-            commentaire.src="/tweet-perso/img/comments.png"
-            commentaire.id = `post${id_post}`
-            document.getElementById(NewPost.id).appendChild(commentaire)
-
-        id_post ++
-    }
-    txt_area.value = ""
-}
 
 
 txt_area.addEventListener("click",function(){
     document.getElementById("zoneEcriture").classList = 'expand'
 })
+
+
+function CreatNewPost (){
+    //si lien video yt alors prévisu de la video like twitter (peut etre api yt)
+    if (txt_area.value != "" | inputImage.value != "" | inputGif.value != ""){
+        creationDiv()
+    }
+    txt_area.value = ""
+    inputImage.value = ""
+    inputGif.value = ""
+}
+
+function creationDiv(){
+    let NewPost 
+    NewPost = document.createElement('div')
+    NewPost.id = `post${id_post}`
+    NewPost.classList.add("tweet")
+
+   
+
+    /*div image profile*/
+    let pp
+    pp = document.createElement('div')
+    pp.classList.add("pp")
+        
+
+        /*div pseudo*/
+        let name
+        name = document.createElement('div')
+        name.classList.add("name")
+        name.innerHTML = `@jesuis${id_post}`
+
+
+        /*div logo pioutter*/
+        let logo
+        logo= document.createElement('img')
+        logo.classList.add("logo")
+        logo.src="/post/img/piout.png"
+
+
+        /*div contenue du tweet*/
+        let content
+        content =document.createElement('div')
+        content.classList.add("contenue")
+
+
+        // ajouter un retour a la ligne
+        if (inputImage.value != ""){
+
+            content.innerHTML +=inputImage.value
+        }
+        if (inputGif.value != ""){
+            content.innerHTML +=inputGif.value
+        }
+
+
+    /*div separartion*/
+    let separation
+    separation= document.createElement('div')
+    separation.classList.add("separation")
+
+
+        /*div like*/
+        let like
+        like= document.createElement('img')
+        like.classList.add("like")
+        like.src="/post/img/iconCoeurLike.png"
+        like.id = `post${id_post}`
+
+
+
+        /*div commentaire*/
+        let commentaire
+        commentaire= document.createElement('img')
+        commentaire.classList.add("commentaire")
+        commentaire.src="/post/img/comments.png"
+        commentaire.id = `post${id_post}`
+
+
+
+    NewPost.appendChild(pp)
+    NewPost.appendChild(name)
+    content.innerHTML = txt_area.value
+    NewPost.appendChild(content)
+    document.getElementById(NewPost.id).appendChild(commentaire)
+    document.getElementById(NewPost.id).appendChild(like)
+    document.getElementById(NewPost.id).appendChild(separation)
+    document.getElementById(NewPost.id).appendChild(logo)
+    document.getElementById('zonePostTweet').appendChild(NewPost)
+    id_post ++
+}
