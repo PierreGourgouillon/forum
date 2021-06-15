80a2f10948c80982fee5df7ac799479f277deed3
arthu <arthur.paturel@ynov.com>
2021-06-15 15:12:07 +0200

    t

--- Front-end/Javascript/footer.js
+++ Front-end/Javascript/footer.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("showMore-filterBox").addEventListener("click", showMore)
+    document.getElementById("showMore-filterBox").addEventListener("click", showMoreFilter)
+    document.getElementById("showMore-sortBox").addEventListener("click", showMoreSort)
 })
 
-function showMore(){
-    let hidden = document.getElementById("hidden")
-    let more_less = document.getElementById("showMore-text")
+function showMoreFilter(){
+    let hidden = document.getElementById("hiddenFilter")
+    let more_less = document.getElementById("showMore-filterText")
+    if(hidden.style.display === "none"){
+        hidden.style.display = ""
+        more_less.innerText = "Voir moins"
+    }else{
+        hidden.style.display = "none"
+        more_less.innerText = "Voir plus"
+    }
+}
+function showMoreSort(){
+    let hidden = document.getElementById("hiddenSort")
+    let more_less = document.getElementById("showMore-sortText")
     if(hidden.style.display === "none"){
         hidden.style.display = ""
         more_less.innerText = "Voir moins"
