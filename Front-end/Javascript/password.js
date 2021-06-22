23d8bf488ee6bdb10d2daeccb3de707f2eced074
tggaule <thomas.gaule@ynov.com>
2021-06-22 14:22:32 +0200

    add oeil mdp

--- Front-end/Javascript/password.js
+++ Front-end/Javascript/password.js
@@ -23,3 +23,39 @@ function testGeneral(){
     console.log("les tests on ete reussie")
     return true
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("eye1").addEventListener("click", AfficherMdp1)
+    document.getElementById("eye2").addEventListener("click", AfficherMdp2)
+    document.getElementById("eye3").addEventListener("click", AfficherMdp3)
+})
+
+function AfficherMdp1() {
+    let input = document.getElementById('passwordActuel');
+
+    if(input.type === "password") {
+        input.type = "text"; 
+    } else {
+        input.type = "password"
+    }
+} 
+
+function AfficherMdp2() {
+    let input = document.getElementById('newPassword');
+    
+    if(input.type === "password") { 
+        input.type = "text"; 
+    } else {
+        input.type = "password"
+    }
+} 
+
+function AfficherMdp3() {
+    let input = document.getElementById('renewPassword');
+    
+    if(input.type === "password") { 
+        input.type = "text"; 
+    } else {
+        input.type = "password"
+    }
+} 
