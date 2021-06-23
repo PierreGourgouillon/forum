a510183e5fc5e3a7b5b77791e1a15fbc5f494fc7
tggaule <thomas.gaule@ynov.com>
2021-06-23 14:31:55 +0200

    modification design settings changePseudo

--- Front-end/Javascript/pseudo.js
+++ Front-end/Javascript/pseudo.js
@@ -1,13 +1,24 @@
-function verifPseudo(){
+
+document.getElementById("submit-button").addEventListener('click', ajoutPseudo())
+
+function ajoutPseudo(){
+    let pseudo = document.getElementById("passwordActuel").value
+    if (pseudo.length >= 6){
+        console.log("pseudo append")
+        changePseudo()
+    }else{
+        console.log(document.getElementById("passwordActuel").value)
+        document.getElementById("passwordActuel").value = ""
+    }
+}
+
+function verificationPseudo(){    
     let pseudo = document.getElementById("passwordActuel").value
     let error = document.getElementById("error")
-    console.log(pseudo)
     if (pseudo.length <= 6){
         error.textContent = "pseudo trop court"
     }else{
-        ("pseudo append")
-        changePseudo()
+        error.textContent = ""
+        console.log("verification validé")
     }
-
-
 }
